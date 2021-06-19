633646e036bfae14448014a24e09de742d3ca61b
yjiakang <1652750913@qq.com>
2021-06-19 18:56:34 +0800

    facet
    add facet

--- R/taxBarPlot.R
+++ R/taxBarPlot.R
@@ -12,39 +12,71 @@
 #' @examples otu_table_L2.txt <- system.file("extdata", "otu_table_L2.txt", package = "microVisu")
 #' @examples design.txt <- system.file("extdata", "design.txt", package = "microVisu")
 #' @examples taxBarPlot(otuTab = otu_table_L2.txt, metaData = design.txt,
-#'  classToPlot = "status", topNum = 10, col = "Set3")
-taxBarPlot  <- function(otuTab, metaData, classToPlot, topNum, col) {
+#'  classToPlot = "status", topNum = 10, col = "Set3", classToFacet = "climate")
+taxBarPlot  <- function(otuTab, metaData, classToPlot, topNum, col, classToFacet = FALSE) {
     # load packages needed
     library("tidyr")
     library("ggplot2")
+    library("reshape2")
+    # color num in the palette
+    if(col %in% (c("Set2", "Pastel2", "Dark2", "Accent"))) {
+        ncolor = 8
+    } else {
+        ncolor = 9
+    }
     otuTab <- read.delim(otuTab, header = TRUE, sep = "\t") # Import otu table
     otuTab <- as.data.frame(t(t(otuTab)/colSums(otuTab)*100)) # Tranfer to percent
     metaData <- read.table(metaData, header = TRUE, row.names = 1, sep = "\t") # Import metadata table
     idx <- intersect(rownames(metaData),colnames(otuTab)) # Find the common samples both in metadata and otu table
     metaData <- metaData[idx,]
     otuTab <- otuTab[,idx]
-    samFile <- as.data.frame(metaData[,classToPlot], row.names = row.names(metaData))
-    colnames(samFile)[1] <- classToPlot
-    otuTab <- merge(samFile, t(otuTab), by = "row.names")[, -1]
-    otuTabMean <- aggregate(otuTab[,-1], by = otuTab[1], FUN = mean) # Calculate the mean of the same group
-    otuTabMeanFinal <- do.call(rbind, otuTabMean)[-1, ]
-    colnames(otuTabMeanFinal) <- otuTabMean[, classToPlot]
-    otuRowName <- row.names(otuTabMeanFinal)
-    otuTabMeanFinal <- as.data.frame(otuTabMeanFinal)
-    otuTabMeanFinal <- as.data.frame(lapply(otuTabMeanFinal, as.numeric))
-    rownames(otuTabMeanFinal) <- otuRowName
-    otuTabMeanFinal$total <- apply(otuTabMeanFinal, 1, sum)
-    otuTabMeanFinal$taxa <- rownames(otuTabMeanFinal)
-    otuTabMeanFinal <- dplyr::arrange(otuTabMeanFinal, desc(total)) # Sort based on the total counts using the imported pkg
-    otuTabMeanFinal <- subset(head(otuTabMeanFinal, n = topNum), select = -total)
-    dataForPlot <- otuTabMeanFinal %>% gather(classToPlot, abundance, -taxa) # Change into long data
-    ggplot(dataForPlot, aes(x = classToPlot, y = abundance, fill = taxa)) +
-        geom_bar(stat = "identity", width = 0.5) +
-        scale_fill_brewer(palette = col) +
-        xlab(NULL) +
-        theme(axis.title = element_text(size = 10, face = "bold"),
-              axis.text.x= element_text(size = 10, face = "bold"))+
-        labs(fill = "Taxonomy") +
-        ylab("Abundance(%)")
+    if(classToFacet == FALSE) {
+        samFile <- as.data.frame(metaData[,classToPlot], row.names = row.names(metaData))
+        colnames(samFile)[1] <- classToPlot
+        otuTab <- merge(samFile, t(otuTab), by = "row.names")[, -1]
+        otuTabMean <- aggregate(otuTab[,-1], by = otuTab[1], FUN = mean) # Calculate the mean of the same group
+        otuTabMeanFinal <- do.call(rbind, otuTabMean)[-1, ]
+        colnames(otuTabMeanFinal) <- otuTabMean[, classToPlot]
+        otuRowName <- row.names(otuTabMeanFinal)
+        otuTabMeanFinal <- as.data.frame(otuTabMeanFinal)
+        otuTabMeanFinal <- as.data.frame(lapply(otuTabMeanFinal, as.numeric))
+        rownames(otuTabMeanFinal) <- otuRowName
+        otuTabMeanFinal$total <- apply(otuTabMeanFinal, 1, sum)
+        otuTabMeanFinal$taxa <- rownames(otuTabMeanFinal)
+        otuTabMeanFinal <- dplyr::arrange(otuTabMeanFinal, desc(total)) # Sort based on the total counts using the imported pkg
+        otuTabMeanFinal <- subset(head(otuTabMeanFinal, n = topNum), select = -total)
+        dataForPlot <- otuTabMeanFinal %>% gather(classToPlot, abundance, -taxa) # Change into long data
+        ggplot(dataForPlot, aes(x = classToPlot, y = abundance, fill = taxa)) +
+            geom_bar(stat = "identity", width = 0.5) +
+            xlab(NULL) +
+            scale_fill_manual(values =  colorRampPalette(brewer.pal(ncolor, col))(topNum)) +
+            theme(axis.title = element_text(size = 10, face = "bold"),
+                  axis.text.x= element_text(size = 10, face = "bold"))+
+            labs(fill = "Taxonomy") +
+            theme_bw() +
+            ylab("Abundance(%)")
+    } else {
+        otuTab$sum <- rowSums(otuTab)
+        otuTab <- arrange(otuTab, desc(sum))
+        otuMeta <- t(subset(head(otuTab, n = topNum), select = -sum)) %>% 
+            as.data.frame %>% 
+            bind_cols(metaData)
+        otuGp <- aggregate(otuMeta[1:topNum], 
+                           by = otuMeta[c(which(colnames(otuMeta)==classToPlot), 
+                                          which(colnames(otuMeta)==classToFacet))],
+                           FUN = mean)
+        otuLong <- melt(otuGp)
+        ggplot(otuLong, aes(x = status, y = value, fill = variable)) +
+            geom_bar(stat = "identity", width = 0.5) +
+            xlab(NULL) +
+            scale_fill_manual(values =  colorRampPalette(brewer.pal(ncolor, col))(topNum)) +
+            theme(axis.title = element_text(size = 10, face = "bold"),
+                  axis.text.x= element_text(size = 10, face = "bold"))+
+            labs(fill = "Taxonomy") +
+            facet_grid(as.formula(paste(".", "~", classToFacet))) + 
+            theme_bw() + 
+            ylab("Abundance(%)")
+    }
+    
 }
 
